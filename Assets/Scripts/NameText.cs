4092952c5eb73b679535e8ae76d48fb5acffdc49
umutcanekinci <umutcannekinci@gmail.com>
2025-05-04 20:23:11 +0300

    Add name color property to NameText class

--- Assets/Scripts/NameText.cs
+++ Assets/Scripts/NameText.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NameText : MonoBehaviour
 {
     [SerializeField] private string objectName;
+    [SerializeField] private Color nameColor = Color.white; // Color of the name text
     [SerializeField] private GameObject nameCanvasPrefab; // Prefab for the name canvas
     private GameObject nameText; // Prefab for the name text
     private GameObject input; // Prefab for the input text
@@ -21,6 +22,7 @@ public class NameText : MonoBehaviour
         nameText = nameCanvas.transform.GetChild(0).gameObject;
         input = nameCanvas.transform.GetChild(1).gameObject; // Get the input text from the name canvas
         nameText.GetComponent<TMP_Text>().text = objectName; // Set the name text to the object's name
+        nameText.GetComponent<TMP_Text>().color = nameColor; // Set the color of the name text
     }
 
     public void Highlight(bool highlight)
